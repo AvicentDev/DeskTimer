33b1f4016da8119f6c874bc5fd024f575f2dbc7f
Alvaro Vicent <avicent@gmail.com>
2025-10-18 17:20:12 +0200

    Prueba

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,16 @@ Route::get('/', function () {
 Route::prefix('auth/google')->group(function () {
     Route::get('redirect',  [GoogleController::class, 'redirectToGoogle']);
     Route::get('callback',  [GoogleController::class, 'handleGoogleCallback']);
+    
+    // Endpoint de prueba
+    Route::get('test', function () {
+        return response()->json([
+            'message' => 'Google OAuth test',
+            'config' => [
+                'client_id' => config('services.google.client_id'),
+                'has_secret' => !empty(config('services.google.client_secret')),
+                'redirect' => config('services.google.redirect'),
+            ]
+        ]);
+    });
 });
